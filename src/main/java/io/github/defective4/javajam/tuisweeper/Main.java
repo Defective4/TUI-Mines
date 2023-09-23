7f0ffbbb9d7e39a89bd049f1428fe84343221af4
Defective <def3ctive4@gmail.com>
2023-09-23 10:52:01 +0200

    Set fullscreen size if using swing terminal emulator

--- src/main/java/io/github/defective4/javajam/tuisweeper/Main.java
+++ src/main/java/io/github/defective4/javajam/tuisweeper/Main.java
@@ -1,15 +1,25 @@
 package io.github.defective4.javajam.tuisweeper;
 
 import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFrame;
 import io.github.defective4.javajam.tuisweeper.core.TUISweeper;
 
+import java.awt.*;
 import java.io.IOException;
 
 public final class Main {
     public static void main(String[] args) {
         try {
-            Screen screen = new DefaultTerminalFactory().createScreen();
+            Terminal term = new DefaultTerminalFactory().createTerminal();
+            if(term instanceof SwingTerminalFrame) {
+                SwingTerminalFrame frame = (SwingTerminalFrame) term;
+                frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+            }
+
+            Screen screen = new TerminalScreen(term);
             screen.startScreen();
             TUISweeper game = new TUISweeper(screen);
             game.start();
